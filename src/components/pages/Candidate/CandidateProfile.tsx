b0993693c2cd1743b74f01100997a6d4b785bf71
se140718 <datlhdse140718@fpt.edu.vn>
2023-02-23 18:38:14 +0700

    information frame

--- src/components/pages/Candidate/CandidateProfile.tsx
+++ src/components/pages/Candidate/CandidateProfile.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import "./CandidateProfile.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket, faPhone, faCakeCandles, faHouse, faVenusMars } from '@fortawesome/free-solid-svg-icons'
 
 export const CandidateProfile: FC = () => {
     return (
@@ -18,9 +18,53 @@ export const CandidateProfile: FC = () => {
                             </div>
                         </div>
                         <div className="col-right">
-                            <div className="col-half">
+                            <div className="col-half mb-50">
                                 <div className="fullname">Đắc Đạt</div>
-                                <button className="btn-edit btn">Edit Profile</button>
+                                <div className="btn-cover">
+                                    <button className="btn-edit btn">Edit Profile</button>
+                                </div>
+                            </div>
+                            <div className="b-0">
+                                <div className="col-half">
+                                    <div className="work-status">
+                                        <strong>N/A</strong>
+                                        <span>On working</span>
+                                    </div>
+                                    <div className="work-status">
+                                        <strong>N/A</strong>
+                                        <span>jobs completed</span>
+                                    </div>
+                                </div>
+                                <div className="col-half">
+                                    <div className="work-status">
+                                        <strong>N/A</strong>
+                                        <span>be hiring</span>
+                                    </div>
+                                    <div className="work-status">
+                                        <strong>N/A</strong>
+                                        <span>repeat hire rate</span>
+                                    </div>
+                                </div>
+                                <div className="col-half">
+                                    <div className="work-status">
+                                        <FontAwesomeIcon icon={faPhone} className="icon m-0" />
+                                        <span>0774816851</span>
+                                    </div>
+                                    <div className="work-status">
+                                        <FontAwesomeIcon m-0 icon={faCakeCandles} className="icon m-0" />
+                                        <span>14/12/2000</span>
+                                    </div>
+                                </div>
+                                <div className="col-half m-0">
+                                    <div className="work-status">
+                                        <FontAwesomeIcon icon={faHouse} className="icon m-0" />
+                                        <span>HCM, Viet Nam</span>
+                                    </div>
+                                    <div className="work-status">
+                                        <FontAwesomeIcon m-0 icon={faVenusMars} className="icon m-0" />
+                                        <span>Male</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
